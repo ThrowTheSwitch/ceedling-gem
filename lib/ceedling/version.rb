5d1b4576fa33ae16d964eee949d118a1ec123265
Greg Williams <williams@atomicembedded.com>
2011-06-11 14:19:36 -0400

    Fixed names of a couple of unit test files in the temp_sensor example. Updated to latest tts libs.

--- lib/ceedling/version.rb
+++ lib/ceedling/version.rb
@@ -2,14 +2,14 @@
 module Ceedling
   module Version
     # @private
-    GEM = "0.0.15"
+    GEM = "0.0.16"
 
     # @private
     CEEDLING = "0.9.200"
     # @private
     CEXCEPTION = "1.2.17"
     # @private
-    CMOCK = "2.0.213"
+    CMOCK = "2.0.214"
     # @private
     UNITY = "2.0.135"
   end
